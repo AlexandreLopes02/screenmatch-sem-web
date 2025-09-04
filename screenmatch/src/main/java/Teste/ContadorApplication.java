39beacd498a6d8477f7d70c048baed5b33a4bda3
AlexandreLopes02 <alenovoxio@gmail.com>
2025-09-04 09:21:39 -0300

    Novas implementações

---
+++ screenmatch/src/main/java/Teste/ContadorApplication.java
@@ -0,0 +1,28 @@
+package Teste;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class ContadorApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContadorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Bem vindo ao contador!");
+        System.out.println("----------------------");
+        System.out.println("Digite um número para contagem: ");
+        Scanner entrada = new Scanner(System.in);
+        var numero = entrada.nextInt();
+
+        for(int i = 1; i<=numero; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
